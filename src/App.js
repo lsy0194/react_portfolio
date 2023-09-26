882ac0141eb6c357767bb1f98c9cfc9a3e6cc415
lsy0194 <tktmadltmddu@naver.com>
2023-09-26 13:25:40 +0900

    전역 스타일 적용

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from './component/common/header/Header';
+import Global from './styles/Global.scss';
+function App() {
+	return (
+		<>
+			<Header />
+		</>
+	);
+}
+
+export default App;
---
+++ src/component/common/header/Header.jsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+import styles from './Header.module.scss';
+console.log(styles);
+//className에 문자가 변수로치환되고 있기 때문에 여러개의 변수값을 클래스로 등록하기 편하고, 클래스연결을 삼항연산자로 조건처리도 가능
+export default function header() {
+	return (
+		<header className={clsx(styles.header)}>
+			<h1>LOGO</h1>
+
+			<ul>
+				<li>Department</li>
+				<li>Gallery</li>
+				<li>Youtube</li>
+				<li>Members</li>
+				<li>Contact</li>
+			</ul>
+		</header>
+	);
+}
